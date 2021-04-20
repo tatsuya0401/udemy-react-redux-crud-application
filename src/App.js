6bad85807773fae738ca9e0f9f09ad8e2fa5bed8
tatsuya0401 <tatsuya.yonehiro@livelynx.co.jp>
2021-04-20 11:08:10 +0900

    create my own React boilerplate

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 
-
-
-
-
-
-
-
-
-import './App.css';
-
-function App() {
-  return (
-    <div className="App"><h1>Hello World!</h1>
-	  </div>
-  );
+class App extends Component {
+	render() {
+  		return <div><h1>llo World!</h1></div>
+	}
 }
 
 export default App;
