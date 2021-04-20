abddb534c31614f03d4759b01bc8f7df368ef7a4
tatsuya0401 <tatsuya.yonehiro@livelynx.co.jp>
2021-04-20 15:25:19 +0900

    redux first commit

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+export const increment = () => ({
+    type: INCREMENT
+});
+export const decrement = () => ({
+    type: DECREMENT
+});
